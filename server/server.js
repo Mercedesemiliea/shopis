4779e6a97e96405efad008ada45b41f22682083e
Mercedesemiliea <mercedesemiliea@hotmail.se>
2024-08-21 12:49:14 +0200

    testa frontend

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'shopis')));
+app.use(express.static(path.join(__dirname, 'frontend')));
 
 app.use('/products', productRouter);
 
