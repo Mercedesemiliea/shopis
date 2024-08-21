38bfd8bf189a01bc194c2ebc64e5c384420c726e
Mercedesemiliea <mercedesemiliea@hotmail.se>
2024-08-21 13:05:50 +0200

    försöker få index sidan att synas

--- server/server.js
+++ server/server.js
@@ -5,14 +5,14 @@ const productRouter = require('./routes/productRouter');
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
 
 
 app.use('/products', productRouter);
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the Shopis API');
-});
+//app.get('/', (req, res) => {
+// res.send('Welcome to the Shopis API');
+//});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
