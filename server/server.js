7b0a90e7ee206191a1c9564656fdf557d961547e
Mercedesemiliea <mercedesemiliea@hotmail.se>
2024-08-22 14:17:16 +0200

    nu då

--- server/server.js
+++ server/server.js
@@ -1,22 +1,10 @@
 const express = require('express');
-//const fs = require('fs');
-const { createProxyMiddleware } = require('http-proxy-middleware');
 const path = require('path');
 const app = express();
-const productRouter = require('./routes/productRouter');
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
-
-
+// Servera statiska filer från 'public' mappen
 app.use(express.static(path.join(__dirname, '..', 'public')));
-
-app.use('/products', productRouter);
-
-app.use('/php', createProxyMiddleware({
-  target: 'http://localhost:8000',
-  changeOrigin: true
-}));
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
