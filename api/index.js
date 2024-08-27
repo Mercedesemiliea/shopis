3311eaf1c3585377ae7689696372ce9f2e67e5cd
Mercedesemiliea <mercedesemiliea@hotmail.se>
2024-08-27 12:09:47 +0200

    express verrcel

--- api/index.js
+++ api/index.js
@@ -4,13 +4,12 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const productRouter = require('./routes/productRouter');
+const productRouter = require('../server/routes/productRouter');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.get('./', (req, res) => {
-    res.sendFile(__dirname + 'api', '/index.php');
-});
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.use('/products', productRouter);
 
