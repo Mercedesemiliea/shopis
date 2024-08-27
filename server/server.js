4f686118b7acb743948af2602c2ba1ada623f73c
Mercedesemiliea <mercedesemiliea@hotmail.se>
2024-08-27 12:42:44 +0200

    bytt namn o så

--- server/server.js
+++ server/server.js
@@ -1,8 +1,25 @@
-const app = require('/server');
+
+const express = require('express');
+const path = require('path');
+const app = express();
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+const productRouter = require('../server/routes/productRouter');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'server', 'index.php'));
 });
 
+app.use('/products', productRouter);
 
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
+
+module.exports = app;
